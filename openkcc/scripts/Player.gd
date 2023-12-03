3b505fd1ab4da11e18f860b436d3b1c453ab7dfb
Nick Maltbie <nick.dmalt@gmail.com>
2023-12-02 20:39:08 -0800

    Added ability to capture player mouse and toggle with Esc Updated player to capture mouse correctly

--- openkcc/scripts/Player.gd
+++ openkcc/scripts/Player.gd
@@ -10,25 +10,31 @@ var mouse_sensibility = 1200
 var mouse_relative_x = 0
 var mouse_relative_y = 0
 
+var allow_movement = true
+
 @onready var cam = $Head/Camera3d as Camera3D
 
 func _ready():
-	# Setup mouse input
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	allow_movement = true
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# If we allow input, let the player jump and move.
+	var direction = Vector3(0, 0, 0)
+	if allow_movement:
+		# Handle Jump.
+		if Input.is_action_just_pressed("Jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("Left", "Right", "Forward", "Back")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var input_dir = Input.get_vector("Left", "Right", "Forward", "Back")
+		direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -38,10 +44,32 @@ func _physics_process(delta):
 
 	move_and_slide()
 
+func capture_mouse():
+	if allow_movement:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
 func _input(event):
+	# On web platform, mouse needs to be clicked in order to be
+	# captured properly. Or as a direct input response like pressing a key.
+	if OS.get_name() == "Web":
+		if event is InputEventMouseButton:
+			allow_movement = true
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+	if event.is_action_pressed("Exit"):
+		# Set mouse to captured mode on input form player
+		# This needs to be done in _input for web support
+		allow_movement = !allow_movement
+		capture_mouse()
+
 	if event is InputEventMouseMotion:
-		rotation.y -= event.relative.x / mouse_sensibility
-		$Head/Camera3d.rotation.x -= event.relative.y / mouse_sensibility
-		$Head/Camera3d.rotation.x = clamp($Head/Camera3d.rotation.x, deg_to_rad(-90), deg_to_rad(90))
-		mouse_relative_x = clamp(event.relative.x, -50, 50)
-		mouse_relative_y = clamp(event.relative.y, -50, 10)
+		# Only rotate player if input is allowed
+		# Adjust rotation based on player input
+		if allow_movement:
+			rotation.y -= event.relative.x / mouse_sensibility
+			$Head/Camera3d.rotation.x -= event.relative.y / mouse_sensibility
+			$Head/Camera3d.rotation.x = clamp($Head/Camera3d.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+			mouse_relative_x = clamp(event.relative.x, -50, 50)
+			mouse_relative_y = clamp(event.relative.y, -50, 10)
