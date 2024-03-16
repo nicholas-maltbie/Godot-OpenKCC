7c3b040f008604ef8514490c6a412ff60d706a2f
Nick Maltbie <nick.dmalt@gmail.com>
2024-03-16 16:00:40 -0700

    Updated player to not slide up walls
    Fixed some style errors

--- openkcc/scripts/Player.gd
+++ openkcc/scripts/Player.gd
@@ -12,12 +12,22 @@ const MAX_SHOVE_RADIANS:float = PI/2
 const EPSILON:float = 0.001
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var gravity:float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+# Direction of up vector
+var up:Vector3 = Vector3.UP
+
+# Velocity due to world forces (like gravity)
+var world_velocity:Vector3 = Vector3.ZERO
+
+# Velocity due to plaeyr input (movement)
+var move_velocity:Vector3 = Vector3.ZERO
+
+# Mouse sensitivity
 var mouse_sensibility = 1200
-var velocity = Vector3.ZERO
 
-var allow_movement = true
+# Allow player movement
+var allow_movement:bool = true
 
 # Grounded configuration
 var grounded_dist = DEFAULT_GROUNDED_HEIGHT
@@ -36,17 +46,78 @@ var _collision:KinematicCollision3D
 
 @onready var cam = $Head/Camera3d as Camera3D
 
-func _ready():
+func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	allow_movement = true
 	_collision = KinematicCollision3D.new();
 
-func check_grounded():
+func _physics_process(_delta) -> void:
+	# Add the gravity.
+	check_grounded()
+	if not is_on_floor() or is_sliding():
+		world_velocity -= up * gravity * _delta
+	elif is_on_floor() and !moving_up():
+		# If player is not moving up and hit the ground, stop world velocity
+		world_velocity = Vector3.ZERO
+
+	# If we allow input, let the player jump and move.
+	var direction = Vector3(0, 0, 0)
+	if allow_movement:
+		# Handle Jump.
+		var jumping := Input.is_action_just_pressed("Jump")
+		if jumping and is_on_floor():
+			world_velocity = up * JUMP_VELOCITY
+
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom
+		# gameplay actions.
+		var input_dir = Input.get_vector("Left", "Right", "Forward", "Back")
+		direction = transform.basis * Vector3(input_dir.x, 0, input_dir.y)
+		direction = direction.normalized()
+
+	if direction:
+		move_velocity.x = direction.x * SPEED
+		move_velocity.z = direction.z * SPEED
+	else:
+		move_velocity.x = move_toward(move_velocity.x, 0, SPEED)
+		move_velocity.z = move_toward(move_velocity.z, 0, SPEED)
+
+	var move:Vector3 = move_velocity
+	if is_on_floor() and not is_sliding():
+		move *= Quaternion(_ground_normal, up)
+
+	move_and_slide(move * _delta)
+	move_and_slide(world_velocity * _delta)
+
+
+func _input(event) -> void:
+	# On web platform, mouse needs to be clicked in order to be
+	# captured properly. Or as a direct input response like pressing a key.
+	if OS.get_name() == "Web":
+		if event is InputEventMouseButton:
+			allow_movement = true
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+	if event.is_action_pressed("Exit"):
+		# Set mouse to captured mode on input form player
+		# This needs to be done in _input for web support
+		allow_movement = !allow_movement
+		capture_mouse()
+
+	if event is InputEventMouseMotion:
+		# Only rotate player if input is allowed
+		# Adjust rotation based on player input
+		if allow_movement:
+			rotation.y -= event.relative.x / mouse_sensibility
+			$Head/Camera3d.rotation.x -= event.relative.y / mouse_sensibility
+			$Head/Camera3d.rotation.x = clamp($Head/Camera3d.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+
+func check_grounded() -> bool:
 	_ground_hit = test_move(global_transform, Vector3.DOWN * grounded_dist, _collision, EPSILON)
 	if _ground_hit:
 		_ground_object = _collision.get_collider()
 		_ground_dist = _collision.get_travel().length()
-		_ground_angle = _collision.get_angle()
+		_ground_angle = _collision.get_normal().angle_to(up)
 		_ground_normal = _collision.get_normal()
 		_ground_position = _collision.get_position()
 		return is_on_floor()
@@ -58,10 +129,16 @@ func check_grounded():
 	_ground_position = Vector3.ZERO
 	return false
 
-func is_on_floor():
-	return _ground_hit and _ground_dist <= grounded_dist and _ground_angle <= max_walk_angle
+func moving_up() -> bool:
+	return world_velocity.dot(up) > 0
 
-func move_and_slide(movement):
+func is_on_floor() -> bool:
+	return _ground_hit and _ground_dist <= grounded_dist
+
+func is_sliding() -> bool:
+	return is_on_floor() and _ground_angle > deg_to_rad(max_walk_angle)
+
+func move_and_slide(movement:Vector3) -> void:
 	var bounce:int = 0;
 	var remaining:Vector3 = movement;
 	var start:Transform3D = global_transform
@@ -91,65 +168,20 @@ func move_and_slide(movement):
 		# Rotate the remaining movement to be projected along the plane
 		# of the surface hit (emulate 'sliding' against the object)
 		remaining = Plane(_collision.get_normal()).project(_collision.get_remainder()).normalized() * remaining_dist
+
+		# If the player is sliding up a wall, stop it
+		# Can check if dot to up vector is almost equal to 0 (perpendicular)
+		if is_on_floor() and not moving_up() and abs(_collision.get_normal().dot(up)) <= EPSILON:
+			# Remove vertical component of remaining movement
+			var up_component := Vector3(up.x * remaining.x, up.y * remaining.y, up.z * remaining.z)
+			remaining -= up_component
+
 		bounce += 1
 
 	global_position = start.origin
 
-func _physics_process(_delta):
-	# Add the gravity.
-	var grounded = check_grounded()
-	if not grounded:
-		velocity.y -= gravity * _delta
-	elif Vector3.UP.dot(velocity) <= 0:
-		velocity.y = 0
-
-	# If we allow input, let the player jump and move.
-	var direction = Vector3(0, 0, 0)
-	if allow_movement:
-		# Handle Jump.
-		if Input.is_action_just_pressed("Jump") and grounded:
-			velocity.y = JUMP_VELOCITY
-
-		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom
-		# gameplay actions.
-		var input_dir = Input.get_vector("Left", "Right", "Forward", "Back")
-		direction = transform.basis * Vector3(input_dir.x, 0, input_dir.y)
-		direction = direction.normalized()
-
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide(velocity * _delta)
-
-func capture_mouse():
+func capture_mouse() -> void:
 	if allow_movement:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	else:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-
-func _input(event):
-	# On web platform, mouse needs to be clicked in order to be
-	# captured properly. Or as a direct input response like pressing a key.
-	if OS.get_name() == "Web":
-		if event is InputEventMouseButton:
-			allow_movement = true
-			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
-	if event.is_action_pressed("Exit"):
-		# Set mouse to captured mode on input form player
-		# This needs to be done in _input for web support
-		allow_movement = !allow_movement
-		capture_mouse()
-
-	if event is InputEventMouseMotion:
-		# Only rotate player if input is allowed
-		# Adjust rotation based on player input
-		if allow_movement:
-			rotation.y -= event.relative.x / mouse_sensibility
-			$Head/Camera3d.rotation.x -= event.relative.y / mouse_sensibility
-			$Head/Camera3d.rotation.x = clamp($Head/Camera3d.rotation.x, deg_to_rad(-90), deg_to_rad(90))
