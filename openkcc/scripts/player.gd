4312e7bef46ae7a82b302774af2a286dd17879bb
Nick Maltbie <nick.dmalt@gmail.com>
2024-03-20 23:19:49 -0700

    Updated player to allow jumping once while sliding
    Player can now jump once while sliding. If player starts sliding after they jumped, they will no longer be allowed to jump until they stop sliding.
    
    Changed player.gd to process inputs via the _input method
    Added helper function to test_helpers.gd to wait for something to be completed in the background
    Added test to verify player can only jump once while sliding

--- openkcc/scripts/player.gd
+++ openkcc/scripts/player.gd
@@ -15,6 +15,16 @@ var mouse_sensibility = 1200
 # Allow player movement
 var allow_movement:bool = true
 
+# Player's move input
+var _input_component_forward:float
+var _input_component_back:float
+var _input_component_left:float
+var _input_component_right:float
+
+# Only allow the player to jump once until they land on solid ground
+var _input_jump:bool = false
+var _can_jump:bool = false
+
 @onready var cam = $Head/Camera3d as Camera3D
 
 func _ready() -> void:
@@ -35,15 +45,14 @@ func _physics_process(_delta) -> void:
 	var direction = Vector3(0, 0, 0)
 	if allow_movement:
 		# Handle Jump.
-		var jumping := Input.is_action_just_pressed("Jump")
-		if jumping and is_on_floor():
-			world_velocity = up * JUMP_VELOCITY
+		_attempt_jump()
 
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom
 		# gameplay actions.
-		var input_dir = Input.get_vector("Left", "Right", "Forward", "Back")
-		direction = transform.basis * Vector3(input_dir.x, 0, input_dir.y)
+		var input_x := _input_component_right - _input_component_left
+		var input_y := _input_component_back - _input_component_forward
+		direction = transform.basis * Vector3(input_x, 0, input_y)
 		direction = direction.normalized()
 
 	if direction:
@@ -60,7 +69,7 @@ func _physics_process(_delta) -> void:
 	move_and_slide(world_velocity * _delta, false)
 	move_and_slide(move * _delta)
 
-func _input(event) -> void:
+func _input(event:InputEvent) -> void:
 	# On web platform, mouse needs to be clicked in order to be
 	# captured properly. Or as a direct input response like pressing a key.
 	if OS.get_name() == "Web":
@@ -74,6 +83,18 @@ func _input(event) -> void:
 		allow_movement = !allow_movement
 		capture_mouse()
 
+	if event.is_action("Jump"):
+		_input_jump = event.is_pressed()
+
+	if event.is_action("Forward"):
+		_input_component_forward = event.get_action_strength("Forward")
+	if event.is_action("Back"):
+		_input_component_back = event.get_action_strength("Back")
+	if event.is_action("Left"):
+		_input_component_left = event.get_action_strength("Left")
+	if event.is_action("Right"):
+		_input_component_right = event.get_action_strength("Right")
+
 	if event is InputEventMouseMotion:
 		# Only rotate player if input is allowed
 		# Adjust rotation based on player input
@@ -81,6 +102,19 @@ func _input(event) -> void:
 			rotation.y -= event.relative.x / mouse_sensibility
 			$Head/Camera3d.rotation.x -= event.relative.y / mouse_sensibility
 			$Head/Camera3d.rotation.x = clamp($Head/Camera3d.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+
+func _attempt_jump():
+	# If the player is on ground and not sliding, allow to jump again
+	if is_on_floor() and not is_sliding():
+		_can_jump = true
+
+	if _input_jump and is_on_floor() and _can_jump:
+		# Toggle jumped flag to prevent jump again until they are back on the ground
+		_can_jump = false
+		_apply_jump()
+
+func _apply_jump():
+	world_velocity = up * JUMP_VELOCITY
 
 func moving_up() -> bool:
 	return world_velocity.dot(up) > 0
--- openkcc/tests/test_player.gd
+++ openkcc/tests/test_player.gd
@@ -15,7 +15,7 @@ var move_params = ParameterFactory.named_parameters(
 		[["Back", "Right"], Vector3.BACK + Vector3.RIGHT],
 	])
 
-var _sender = InputSender.new(Input)
+var _sender
 var _character:Player
 var _ground:StaticBody3D
 
@@ -42,7 +42,8 @@ func before_each():
 	head.add_child(camera)
 	add_child_autofree(_character)
 
-	_ground = TestUtils.add_wall(self, Vector3(10, 1, 10), Vector3(0, -0.501, 0), Vector3.ZERO, Vector3.ZERO, "Ground")
+	_ground = TestUtils.add_wall(self, Vector3(100, 1, 100), Vector3(0, -0.501, 0), Vector3.ZERO, Vector3.ZERO, "Ground")
+	_sender = InputSender.new(_character)
 
 func after_each():
 	_sender.release_all()
@@ -56,10 +57,10 @@ func after_all():
 func test_player_move(params=use_parameters(move_params)):
 	var start:Vector3 = _character.global_position
 	for dir in params.input_array:
-		Input.action_press(dir)
+		_sender.action_down(dir)
 	simulate(_character, 30, 1.0/30)
 	for dir in params.input_array:
-		Input.action_release(dir)
+		_sender.action_up(dir)
 	var current:Vector3 = _character.global_position
 	var delta:Vector3 = current - start
 	assert_almost_eq(delta.dot(params.movement_dir.normalized()), Player.SPEED, 0.1)
@@ -69,19 +70,50 @@ func test_player_move(params=use_parameters(move_params)):
 ## Verify that when the player jumps their vertical velocity is set to
 ## the jump velocity of the player.
 func test_player_jump():
-	simulate(_character, 1, 1.0/30.0)
 	assert_almost_eq(_character.world_velocity.length(), 0.0, 0.01)
-	Input.action_press("Jump")
-	simulate(_character, 1, 1.0/30.0)
+	_sender.action_down("Jump")
+	simulate(_character, 1, 1)
 	assert_almost_eq(_character.world_velocity.dot(Vector3.UP), Player.JUMP_VELOCITY, 0.01)
-	Input.action_release("Jump")
+	_sender.action_up("Jump")
 
 ## Test player fall by moving them up into the air and assert that
 ## they fall down at least 10 units within 2 seconds and stop when they hit the ground.
 func test_player_fall():
-	_character.global_position = Vector3.UP * 11
+	_character.global_position = Vector3.UP * 10
 	var start:Vector3 = _character.global_position
 	simulate(_character, 120, 1.0/30)
 	var current:Vector3 = _character.global_position
 	var delta:Vector3 = current - start
 	assert_almost_eq(delta.dot(Vector3.DOWN), 10.0, 0.1)
+
+## Put the player on a sliding surface
+## Assert that they can only jump once even if they
+## are back on the ground
+func test_player_jump_once():
+	# have player stand on flat surface
+	await TestUtils.wait_until_async(self, _character.is_on_floor, _character)
+	assert_true(_character._can_jump)
+
+	# Make surface player is standing on slanted ground and they are sliding down
+	_ground.global_rotation_degrees = Vector3(0, 0, 65)
+	_ground.global_position = Vector3(0, -1.6, 0)
+
+	# Wait until player is back on floor and sliding
+	await TestUtils.wait_until_async(self, _character.is_sliding, _character, 10, 0.1)
+
+	# Once player is sliding, have them attempt a jump
+	assert_true(_character._can_jump)
+	assert_false(_character.moving_up())
+
+	# After sending jump input, player should be moving up
+	_sender.action_down("Jump").wait_frames(1)
+	await _sender.idle
+	await TestUtils.wait_until_async(self, _character.moving_up, _character)
+
+	# Player should no longer be able to jump when they are in the air
+	assert_false(_character._can_jump)
+	_sender.action_up("Jump")
+
+	# Wait until they are on the ground again, they should still no longer be able to jump
+	await TestUtils.wait_until_async(self, _character.is_on_floor, _character, 10, 0.1)
+	assert_false(_character._can_jump)
