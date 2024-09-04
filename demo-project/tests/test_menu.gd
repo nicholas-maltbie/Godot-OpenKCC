17b032075ca075f728b150a63f285f79913f10a1
Nick Maltbie <nick.dmalt@gmail.com>
2024-09-04 01:54:04 -0700

    Added basic menu tests

---
+++ demo-project/tests/test_menu.gd
@@ -0,0 +1,50 @@
+extends GutTest
+
+const MenuManager = preload("res://scripts/menu_manager.gd")
+const menu_scene = preload("res://scenes/menu.tscn")
+
+var _menu:MenuManager
+var _sender
+
+func before_all():
+	pass
+
+func before_each():
+	_menu = menu_scene.instantiate()
+	_sender = InputSender.new(_menu)
+	_menu._ready()
+
+func after_each():
+	_sender.release_all()
+	_sender.clear()
+	_menu.free()
+
+func after_all():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func test_menu_toggle():
+	# Assert that menu starts off closed
+	print(_menu.actions.visible)
+	assert_false(_menu.actions.visible)
+	assert_false(_menu.menu_open)
+	
+	# Toggle manually via input bus
+	MenuBus.toggle_menu.emit()
+	assert_true(_menu.actions.visible)
+	assert_true(_menu.menu_open)
+	assert_eq(Input.get_mouse_mode(), Input.MOUSE_MODE_VISIBLE)
+	MenuBus.toggle_menu.emit()
+	assert_false(_menu.actions.visible)
+	assert_false(_menu.menu_open)
+	assert_eq(Input.get_mouse_mode(), Input.MOUSE_MODE_CAPTURED)
+
+func test_menu_on_input():
+	# Assert that menu starts off closed
+	print(_menu.actions.visible)
+	assert_false(_menu.menu_open)
+	
+	# Toggle menu via input
+	_sender.action_down("Exit")
+	assert_true(_menu.menu_open)
+	_sender.action_down("Exit")
+	assert_false(_menu.menu_open)
