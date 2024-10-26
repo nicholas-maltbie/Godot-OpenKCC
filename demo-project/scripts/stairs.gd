746c007b07b42fc952044d38214a8590060695ee
Nick Maltbie <nick.dmalt@gmail.com>
2024-10-25 19:51:59 -0700

    Added Optimization for Stairs Script (#35)
    * Optimized stairs script to not generate duplicate objects
    * Added options for including side, back, and bottom faces for staircase
    * Updated UT accordingly for the new features
    * Refactored some of the code in the stairs script to make it a bit
    easier to read

--- demo-project/scripts/stairs.gd
+++ demo-project/scripts/stairs.gd
@@ -7,135 +7,150 @@ extends MeshInstance3D
 	get:
 		return num_step
 	set(value):
-		if value > 0 and value != num_step:
-			num_step = value
-			if update_on_set:
-				_build_mesh()
+		if value <= 0:
+			return
+		var previous = num_step
+		num_step = value
+		_build_on_set(previous, num_step)
 
 ## Height of each step
 @export var step_height:float = 0.2:
 	get:
 		return step_height
 	set(value):
-		if value != step_height:
-			step_height = value
-			if update_on_set:
-				_build_mesh()
+		var previous = step_height
+		step_height = value
+		_build_on_set(previous, step_height)
 
 ## Depth of each step
 @export var step_depth:float = 0.35:
 	get:
 		return step_depth
 	set(value):
-		if value != step_depth:
-			step_depth = value
-			if update_on_set:
-				_build_mesh()
+		var previous = step_depth
+		step_depth = value
+		_build_on_set(previous, step_depth)
 
 ## Width of each step
 @export var step_width:float = 1:
 	get:
 		return step_width
 	set(value):
-		if value != step_width:
-			step_width = value
-			if update_on_set:
-				_build_mesh()
+		var previous = step_width
+		step_width = value
+		_build_on_set(previous, step_width)
+
+## Include sides for each step
+@export var include_side_faces:bool = true:
+	get:
+		return include_side_faces
+	set(value):
+		var previous = include_side_faces
+		include_side_faces = value
+		_build_on_set(previous, include_side_faces)
+
+## Include square for back of staircase
+@export var include_back_face:bool = true:
+	get:
+		return include_back_face
+	set(value):
+		var previous = include_back_face
+		include_back_face = value
+		_build_on_set(previous, include_back_face)
+
+## Include bottom face for staircase
+@export var include_bottom_face:bool = false:
+	get:
+		return include_bottom_face
+	set(value):
+		var previous = include_bottom_face
+		include_bottom_face = value
+		_build_on_set(previous, include_bottom_face)
 
 ## Texture for stairs
 @export var texture:Texture2D:
 	get:
 		return texture
 	set(value):
-		if value != texture:
-			texture = value
-			if update_on_set:
-				_build_mesh()
+		var previous = texture
+		texture = value
+		_build_on_set(previous, value)
 
-## Object for the stairs as a child to this
-@export var _stairs_obj:Node3D
-
-var update_on_set=true
+## Debug value to control whether the mesh is updated
+## time a value is set.
+var _update_on_set=true
 
 func force_update_mesh() -> void:
-	if _stairs_obj != null:
-		_stairs_obj.free()
-
+	# Setup arrays for vertices nad UF
 	var vertices = PackedVector3Array()
 	var uvs = PackedVector2Array()
-	var mat = StandardMaterial3D.new()
-	var color = Color(1, 1, 1)
-	mat.albedo_color = color
-	mat.albedo_texture = texture
 
-	var offset = Vector2(step_width, 1)
+	# Setup material
+	var mat = StandardMaterial3D.new()
+	mat.albedo_color = Color(1, 1, 1)
+	mat.albedo_texture = texture
+	mat.vertex_color_use_as_albedo = true
+
+	# Build mesh step by step
+	var uv_offset = Vector2(step_width, 1)
+	var left:float = 0
+	var right:float = step_width
 	for step in num_step:
 		# Build each step as two sets of squares
 		# one on the front, one on the back
-		var left:float = 0
-		var right:float = step_width
-		var top:float = (step + 1) * step_height
 		var bottom:float = step * step_height
+		var top:float = (step + 1) * step_height
 		var front:float = step * step_depth
 		var back:float = (step + 1) * step_depth
 
-		var front_bottom_line := Line3D.new(Vector3(left, bottom, front), Vector3(right, bottom, front))
-		var front_top_line := Line3D.new(Vector3(left, top, front), Vector3(right, top, front))
-		var back_top_line := Line3D.new(Vector3(left, top, back), Vector3(right, top, back))
-		var base_back_line := Line3D.new(Vector3(left, 0, back), Vector3(right, 0, back))
-		var base_front_line := Line3D.new(Vector3(left, 0, front), Vector3(right, 0, front))
-
 		# Add front face
-		_add_square(vertices, front_bottom_line.v2, front_top_line.v2, front_top_line.v1, front_bottom_line.v1)
-		_add_square_uv(uvs, \
-			Vector2(front_bottom_line.v2.x, front_bottom_line.v2.y), \
-			Vector2(front_top_line.v2.x, front_top_line.v2.y), \
-			Vector2(front_top_line.v1.x, front_top_line.v1.y), \
-			Vector2(front_bottom_line.v1.x, front_bottom_line.v1.y), \
-			offset)
+		_add_square(vertices, Vector3(right, bottom, front), Vector3(right, top, front), \
+			Vector3(left, top, front), Vector3(left, bottom, front))
+		_add_square_uv(uvs, Vector2(right, bottom), Vector2(right, top), \
+			Vector2(left, top), Vector2(left, bottom), uv_offset)
 
 		# Add top face
-		_add_square(vertices, front_top_line.v2, back_top_line.v2, back_top_line.v1, front_top_line.v1)
-		_add_square_uv(uvs, \
-			Vector2(front_top_line.v2.x, front_top_line.v2.z), \
-			Vector2(back_top_line.v2.x, back_top_line.v2.z), \
-			Vector2(back_top_line.v1.x, back_top_line.v1.z), \
-			Vector2(front_top_line.v1.x, front_top_line.v1.z), \
-			offset)
+		_add_square(vertices, Vector3(right, top, front), Vector3(right, top, back), \
+			Vector3(left, top, back), Vector3(left, top, front))
+		_add_square_uv(uvs, Vector2(right, front), Vector2(right, back), \
+			Vector2(left, back), Vector2(left, front), uv_offset)
 
 		# Add side faces
-		_add_square(vertices, front_top_line.v1, back_top_line.v1, base_back_line.v1, base_front_line.v1)
-		_add_square(vertices, base_front_line.v2, base_back_line.v2, back_top_line.v2, front_top_line.v2)
-		_add_square_uv(uvs, \
-			Vector2(front_top_line.v2.z, front_top_line.v2.y), \
-			Vector2(back_top_line.v2.z, back_top_line.v2.y), \
-			Vector2(base_back_line.v1.z, base_back_line.v1.y), \
-			Vector2(base_front_line.v1.z, base_front_line.v1.y), \
-			offset)
-		_add_square_uv(uvs, \
-			Vector2(base_front_line.v2.z, base_front_line.v2.y), \
-			Vector2(base_back_line.v2.z, base_back_line.v2.y), \
-			Vector2(back_top_line.v1.z, back_top_line.v1.y), \
-			Vector2(front_top_line.v1.z, front_top_line.v1.y), \
-			offset)
+		if include_side_faces:
+			# Left side
+			_add_square(vertices, Vector3(left, top, front), Vector3(left, top, back), \
+				Vector3(left, 0, back), Vector3(left, 0, front))
+			_add_square_uv(uvs, Vector2(front, top), Vector2(back, top), \
+				Vector2(back, 0), Vector2(front, 0), uv_offset)
 
-		# Add back face if last step
+			# Right side
+			_add_square(vertices, Vector3(right, 0, front), Vector3(right, 0, back), \
+				Vector3(right, top, back), Vector3(right, top, front))
+			_add_square_uv(uvs, Vector2(front, 0), Vector2(back, 0), \
+				Vector2(back, top), Vector2(front, top), uv_offset)
+
+		# If on the last step
 		if step == num_step - 1:
-			_add_square(vertices, base_back_line.v1, back_top_line.v1, back_top_line.v2, base_back_line.v2)
-			_add_square_uv(uvs, \
-				Vector2(base_back_line.v2.x, base_back_line.v2.y), \
-				Vector2(back_top_line.v2.x, back_top_line.v2.y), \
-				Vector2(back_top_line.v1.x, back_top_line.v1.y), \
-				Vector2(base_back_line.v1.x, base_back_line.v1.y), \
-				offset)
+			# Add back face if last step
+			if include_back_face:
+				_add_square(vertices, Vector3(left, 0, back), Vector3(left, top, back), \
+					Vector3(right, top, back), Vector3(right, 0, back))
+				_add_square_uv(uvs, Vector2(right, 0), Vector2(right, top), \
+					Vector2(left, top), Vector2(left, 0), uv_offset)
 
+			# Add bottom face if needed
+			if include_bottom_face:
+				_add_square(vertices, Vector3(left, 0, 0), Vector3(left, 0, back), \
+					Vector3(right, 0, back), Vector3(right, 0, 0))
+				_add_square_uv(uvs, Vector2(right, 0), Vector2(right, back), \
+					Vector2(left, back), Vector2(left, 0), uv_offset)
+
+	# Build mesh from vertices and UV values
 	var st = SurfaceTool.new()
 	st.begin(Mesh.PRIMITIVE_TRIANGLES)
 	st.set_material(mat)
-	mat.vertex_color_use_as_albedo = true
-
 	for idx in vertices.size():
-		st.set_color(color)
+		st.set_color(mat.albedo_color)
 		st.set_uv(uvs[idx])
 		st.add_vertex(vertices[idx])
 
@@ -144,31 +159,45 @@ func force_update_mesh() -> void:
 	st.generate_tangents()
 	self.mesh = st.commit()
 
-	var static_body = StaticBody3D.new()
-	var collision_body = CollisionShape3D.new()
+	var static_body:StaticBody3D = null
+	var collision_body:CollisionShape3D = null
+
+	for child in get_children():
+		if child is StaticBody3D:
+			static_body = child
+			break
+
+	if static_body == null:
+		static_body = StaticBody3D.new()
+		add_child(static_body)
+
+	for child in static_body.get_children():
+		if child is CollisionShape3D:
+			collision_body = child
+			break
+
+	if collision_body == null:
+		collision_body = CollisionShape3D.new()
+		static_body.add_child(collision_body)
+
 	var shape_3d = ConcavePolygonShape3D.new()
 	shape_3d.set_faces(vertices)
 	collision_body.shape = shape_3d
-
-	add_child(static_body)
-	static_body.add_child(collision_body)
 
 	if Engine.is_editor_hint():
 		static_body.owner = get_tree().edited_scene_root
 		collision_body.owner = get_tree().edited_scene_root
 
-	var existing = find_child("StairsBody")
-	if existing != null:
-		existing.free()
-
-	static_body.name = "StairsBody"
-	_stairs_obj = static_body
-
+## Upon object entering the scene, build the mesh.
 func _enter_tree() -> void:
-	if _stairs_obj != null and _stairs_obj.get_parent() != self:
-		_stairs_obj = find_child("StairsBody")
-	_build_mesh_if_none_exists()
+	_build_mesh()
 
+## Check if property has changed and update if configured.
+func _build_on_set(previous, new) -> void:
+	if previous != new and _update_on_set:
+		_build_mesh()
+
+## Helper method to add a square to a set of PackedVector3Array as two triangles.
 func _add_square(vertices:PackedVector3Array, v1:Vector3, v2:Vector3, v3:Vector3, v4:Vector3) -> void:
 	vertices.push_back(v1)
 	vertices.push_back(v2)
@@ -178,29 +207,32 @@ func _add_square(vertices:PackedVector3Array, v1:Vector3, v2:Vector3, v3:Vector3
 	vertices.push_back(v4)
 	vertices.push_back(v1)
 
-func _add_square_uv(uvs:PackedVector2Array, v1:Vector2, v2:Vector2, v3:Vector2, v4:Vector2, offset:Vector2) -> void:
-	uvs.push_back(offset - v1)
-	uvs.push_back(offset - v2)
-	uvs.push_back(offset - v3)
+## Helper method to add a square to a UV map as two triangles.
+func _add_square_uv(uvs:PackedVector2Array, v1:Vector2, v2:Vector2, v3:Vector2, v4:Vector2, uv_offset:Vector2) -> void:
+	uvs.push_back(uv_offset - v1)
+	uvs.push_back(uv_offset - v2)
+	uvs.push_back(uv_offset - v3)
 
-	uvs.push_back(offset - v3)
-	uvs.push_back(offset - v4)
-	uvs.push_back(offset - v1)
+	uvs.push_back(uv_offset - v3)
+	uvs.push_back(uv_offset - v4)
+	uvs.push_back(uv_offset - v1)
 
-func _build_mesh_if_none_exists() -> void:
-	if _stairs_obj == null:
-		_build_mesh()
-
+## Create mesh if node is ready, cancel otherwise.
 func _build_mesh() -> void:
 	if !is_node_ready():
 		return
 
 	force_update_mesh()
 
+## Helper class for managing a line in 3D space.
 class Line3D:
+	## Starting point of line.
 	var v1:Vector3
+
+	## Ending point of line.
 	var v2:Vector3
 
+	## Creates a new instance of Line3D with a given start and ending point.
 	func _init(start:Vector3, end:Vector3):
 		self.v1 = start
 		self.v2 = end
